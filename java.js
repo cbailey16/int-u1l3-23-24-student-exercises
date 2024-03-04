a7449e8b84670a1f6716fb71146ca3a5c188a083
cbailey16 <143618645+cbailey16@users.noreply.github.com>
2024-03-04 15:39:00 +0000

    monday 3/4

---
+++ java.js
@@ -0,0 +1,22 @@
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let button = document.querySelector(".button");
+let results = document.querySelector(".results");
+
+button.onclick=function(){
+let answer1 = input1.value;
+let answer2 = input2.value;
+
+if (answer1 === "fast" && answer2 === "yes"){
+}
+console.log("cat")
+if (answer1 === "fast" && answer2 === "no"){
+
+}
+if (answer1 === "slow" && answer2 === "yes"){
+
+}
+if (answer1 === "slow" && answer2 === "no"){
+    console.log("frog")
+}
+}
