0d135d92804995d1de0656810b8cd6103110c95f
cbailey16 <143618645+cbailey16@users.noreply.github.com>
2024-03-11 14:37:32 +0000

    3/11/24

--- java.js
+++ java.js
@@ -1,22 +1,14 @@
-let input1 = document.querySelector(".input1");
-let input2 = document.querySelector(".input2");
+let input1 = document.querySelector(".input1").value;
+let input2 = document.querySelector(".input2").value;
 let button = document.querySelector(".button");
 let results = document.querySelector(".results");
 
 button.onclick=function(){
-let answer1 = input1.value;
-let answer2 = input2.value;
+    console.log("button check");
+}
 
-if (answer1 === "fast" && answer2 === "yes"){
-}
-console.log("cat")
-if (answer1 === "fast" && answer2 === "no"){
 
-}
-if (answer1 === "slow" && answer2 === "yes"){
+if (answer1 === "fast" && answer2 === "yes"){results.innerHTML = "Check the password";}
 
-}
-if (answer1 === "slow" && answer2 === "no"){
-    console.log("frog")
-}
-}
+console.log("cat");
+
