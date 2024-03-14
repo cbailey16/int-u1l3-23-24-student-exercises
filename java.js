34a9178de389e7c5b90e27ba810669c8fea311ab
cbailey16 <143618645+cbailey16@users.noreply.github.com>
2024-03-14 14:40:05 +0000

    3/14/24

--- java.js
+++ java.js
@@ -1,14 +1,29 @@
-let input1 = document.querySelector(".input1").value;
-let input2 = document.querySelector(".input2").value;
-let button = document.querySelector(".button");
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let button = document.querySelector("button");
+console.log("button");
+console.log("apple");
 let results = document.querySelector(".results");
+let picture = document.querySelector(".picture");
+button.onclick = function(){
 
-button.onclick=function(){
-    console.log("button check");
+    console.log("click check");
+
+    let answer1 = input1.value;
+    let answer2 = input2.value;
+   
+
+if (answer1 === "Y" && answer2 === "Y"){
+    console.log("check");
+
+    //results.innerHTML = "ur mom";
+    results.innerHTML= ("img").src="buff_frog.png";
+    } 
+
+
+//else if (answer1 === "Y" && answer2 ==="N"){
+  //  console.log("hi")
 }
 
 
-if (answer1 === "fast" && answer2 === "yes"){results.innerHTML = "Check the password";}
-
-console.log("cat");
 
